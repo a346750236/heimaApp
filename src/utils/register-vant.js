5f54dd2efcdab3588b66594de4c9afd3999b27ec
qiaolei <a346750236@vip.qq.com>
2020-01-11 16:13:10 +0800

    处理登录和未登录

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -11,7 +11,10 @@ import {
   Toast,
   CountDown,
   Tabbar,
-  TabbarItem
+  TabbarItem,
+  Image,
+  Grid,
+  GridItem
 } from 'vant'
 
 Vue.use(Button)
@@ -24,3 +27,6 @@ Vue.use(Button)
   .use(CountDown)
   .use(Tabbar)
   .use(TabbarItem)
+  .use(Image)
+  .use(Grid)
+  .use(GridItem)
